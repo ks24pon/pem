c761fa5b4f4e0762a40e7fefbe2a8a0d0d20286e
渡部怜央 <pom123re0624@gmail.com>
2023-04-04 08:33:22 +0900

    column追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_03_011837) do
+ActiveRecord::Schema.define(version: 2023_04_03_135539) do
 
   create_table "articles", charset: "utf8", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2023_04_03_011837) do
     t.string "kind"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "dog_name"
   end
 
 end
