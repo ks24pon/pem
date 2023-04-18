25d24c8bfa6b96d5fef38d8dccc9ba94f3bf6575
ks24pon <pom123re0624@gmail.com>
2023-04-18 17:48:15 +0900

    4/18:いいね表示

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_secure_password
 	has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+  # 紐付け
+  has_many :likes
+  #likesテーブルにarticle_idが存在するか
+  def liked_by?(article_id)
+    # likeモデルからいいねが存在するかどうか
+    likes.where(article_id: article_id).exists?
+  end
 end
