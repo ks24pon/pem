52eaa145562ef6f2cf88195a0c21ee74f8049df0
ks24pon <pom123re0624@gmail.com>
2023-05-08 18:02:08 +0900

    5/8:バリデーション表示

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -4,6 +4,21 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   # 紐付け
   has_many :likes
+  # バリデーション
+  # ニックネーム
+  validates :name, presence: true
+  # メールアドレス
+  validates :email, presence: true
+  # 年齢
+  validates :age, presence: true
+  # 性別
+  validates :sex, presence: true
+  # 地域
+  validates :live, presence: true
+  # パスワード inで8~32文字内
+  validates :password, length: { in: 8..32 }
+  # パスワード(再確認)
+  validates :password_confirmation, presence: true
   #likesテーブルにarticle_idが存在するか
   def liked_by?(article_id)
     # likeモデルからいいねが存在するかどうか
